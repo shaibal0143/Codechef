a84f5c00fe8dbe632f591773e28dffcd79a40733
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-16 00:09:38 +0530

    Add files via upload

---
+++ biryani_classes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class biryani_classes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int s = x * y;
+
+            System.out.println(s);
+
+        }
+    }
+
+}
