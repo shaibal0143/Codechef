1f7b963abe091ed1e118bf6142e670fa6a324d41
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-15 11:06:17 +0530

    Add files via upload

---
+++ chess.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class chess {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int n = scan.nextInt();
+            int min = n * 60;
+            int times = min / 20;
+            System.out.println(times);
+        }
+
+    }
+
+}
---
+++ coins_needed.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class coins_needed {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int n = scan.nextInt();
+            int c = n % 10;
+            System.out.println(c);
+        }
+    }
+
+}
