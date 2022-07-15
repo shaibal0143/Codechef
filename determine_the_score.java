9eda94f36f97e058e214e692c1b870977414d29f
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-16 00:10:44 +0530

    Add files via upload

---
+++ determine_the_score.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class determine_the_score {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int x = scan.nextInt();
+            int n = scan.nextInt();
+            int s = x / 10;
+            // int sc = s * n;
+            System.out.println(s * n);
+
+        }
+    }
+
+}
