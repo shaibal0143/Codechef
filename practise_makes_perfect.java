160af58055be7094eadb9bf9cef5d479b526e377
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-17 00:14:46 +0530

    Add files via upload

---
+++ practise_makes_perfect.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class practtise_makes_perfect {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int prob[] = new int[4];
+        for (int i = 0; i < 4; i++) {
+            prob[i] = scan.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < prob.length; i++) {
+            if (prob[i] >= 10) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
