2eb8e759a29e3449d4386cb4d61f531836fafc71
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-18 10:19:02 +0530

    Add files via upload

---
+++ best_of_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class best_of_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            if (a > b) {
+                System.out.println(a);
+            } else {
+                System.out.println(b);
+            }
+        }
+    }
+
+}
