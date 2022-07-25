28a9619817918bf7229040b8de885a52ffbb1271
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-25 19:44:06 +0530

    Add files via upload

---
+++ buylamps.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class buylamps {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int k = scan.nextInt();
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            if (x > y) {
+                System.out.println((n - k) * y + k * x);
+            } else {
+                System.out.println(n * x);
+            }
+
+        }
+        scan.close();
+
+    }
+
+}
