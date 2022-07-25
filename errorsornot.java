5ce45c6c6566851dd9f90dd5548dab2c0dff7fb8
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-25 19:44:40 +0530

    Add files via upload

---
+++ errorsornot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class errorsornot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int v = scan.nextInt();
+            if (n >= v) {
+                System.out.println("no");
+            } else {
+                System.out.println("yes");
+            }
+        }
+        scan.close();
+    }
+
+}
