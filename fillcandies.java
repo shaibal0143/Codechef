3651e432fae0c24bb3f06a40a0e09168e0bb91d8
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-25 19:43:36 +0530

    Add files via upload

---
+++ fillcandies.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fillcandies {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();// total no. of candies
+            int k = scan.nextInt();// total no of pockets
+            int m = scan.nextInt();// each pocket can hold
+            int p = k * m;
+            if (n < p) {
+                System.out.println("1");
+            } else if (n % p > 0) {
+                System.out.println(n / (k * m) + 1);
+            } else if (n % p == 0) {
+                System.out.println(n / (k * m));
+            }
+        }
+        scan.close();
+    }
+
+}
