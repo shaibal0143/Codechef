d4ca92523d292a72c481ed475bea994254cc40df
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-28 20:40:27 +0530

    Add files via upload

---
+++ hotorcold.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hotorcold {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            if (x >= 20) {
+                System.out.println("hot");
+            } else {
+                System.out.println("cold");
+            }
+        }
+        scan.close();
+    }
+
+}
