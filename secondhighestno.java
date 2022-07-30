4501336466d16583adc14a7a52bedbf6944a7003
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-30 20:35:15 +0530

    Add files via upload

---
+++ secondhighestno.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class secondhighestno {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            if ((a > b && a < c) || (a < b && a > c)) {
+                System.out.println(a);
+            } else if (b > c && b < a || b < c && b > a) {
+                System.out.println(b);
+            } else {
+                System.out.println(c);
+            }
+        }
+    }
+
+}
