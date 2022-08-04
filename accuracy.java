726029350c42da478d801e5042049738cc837f9e
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-04 23:03:39 +0530

    Add files via upload

---
+++ accuracy.java
@@ -0,0 +1,23 @@
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
+
+public class accuracy {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int mid = (x + y) / 2;
+            if (x > mid) {
+                int dig = x - mid;
+                System.out.println(dig);
+            } else {
+                int dig1 = y - mid;
+                System.out.println(dig1);
+            }
+        }
+        scan.close();
+    }
+
+}
