4974516e15ff65ea02bab8c2851cf768447b5516
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-04 23:03:53 +0530

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Discount {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            int dis = (100 - x);
+            System.out.println(dis);
+        }
+        scan.close();
+    }
+
+}
