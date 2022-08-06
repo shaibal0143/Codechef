f49f0587906947398e120c542283cb8f4b80d0b4
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-06 11:31:04 +0530

    Update age_limit.java

--- age_limit.java
+++ age_limit.java
@@ -5,7 +5,7 @@ public class age_limit {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         for (int i = 1; i <= t; i++) {
-            int x = scan.nextInt();
+            int x  = scan.nextInt();
             int y = scan.nextInt();
             int a = scan.nextInt();
             if (a >= x && a < y) {
