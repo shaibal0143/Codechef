97ce18a256a3c3051ffe5b197505f0d895ba9244
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-07 23:19:21 +0530

    Update passing_exam.java

--- passing_exam.java
+++ passing_exam.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class passing_exam {
+public class passing_exam  {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
