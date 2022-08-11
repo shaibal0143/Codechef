ddb2176f8ac747825da7344e663811f325d670be
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-11 19:15:52 +0530

    Add files via upload

---
+++ final_population.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class final_population {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int z = scan.nextInt();
+            System.out.println(x - y + z);
+        }
+    }
+
+}
