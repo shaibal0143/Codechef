c281067e663580ae0d315ed08839d0514e8ac26f
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-11 19:13:49 +0530

    Add files via upload

---
+++ best_price.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class best_price {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int z = scan.nextInt();
+            int a = scan.nextInt();
+            int sum1 = (x - z);
+            int sum2 = (y - a);
+            if (sum1 > sum2) {
+                System.out.println("second");
+            } else {
+                System.out.println("first");
+            }
+        }
+    }
+
+}
