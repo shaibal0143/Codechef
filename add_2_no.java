e2387fae64e4c4446b2cd7c6c9438f70d8f903ec
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-15 19:43:11 +0530

    Update add_2_no.java

--- add_2_no.java
+++ add_2_no.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class add_2_no  {
+public class add_2_no   {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
