f787c6b7278ef16c692c5d26282a9f5fbcbbe11e
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-20 09:39:03 +0530

    Update fillcandies.java

--- fillcandies.java
+++ fillcandies.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class fillcandies {
+public class  fillcandies {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
