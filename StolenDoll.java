f1c30258d89d8cfd5fde4472460c74a17eef3393
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-21 10:15:11 +0530

    Add files via upload

---
+++ StolenDoll.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StolenDoll {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+            int x = 0;
+            for (int i = 0; i < n; i++) {
+                x = x ^ arr[i];
+            }
+            System.out.println(x);
+
+        }
+        scan.close();
+    }
+
+}
