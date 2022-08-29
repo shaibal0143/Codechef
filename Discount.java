a7a414dd7a84505250a0403e977c228673ac8559
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-29 19:33:00 +0530

    Update Discount.java

--- Discount.java
+++ Discount.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class  Discount {
+public class Discount {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
