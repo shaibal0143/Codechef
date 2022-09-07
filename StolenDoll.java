ba5f469f438d1b59e1ebc0fc84555f6264c11548
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-09-07 19:41:39 +0530

    Update StolenDoll.java

--- StolenDoll.java
+++ StolenDoll.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class StolenDoll {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         while (t-- > 0) {
