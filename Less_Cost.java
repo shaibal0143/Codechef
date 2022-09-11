0cb2ed5dc155ddedaeb4a8bc9068090c3b538341
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-09-11 20:55:36 +0530

    Update Less_Cost.java

--- Less_Cost.java
+++ Less_Cost.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Less_Cost {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         while (t-- > 0) {
