04d7ed75b5332953418599a3119b5b7041cc2e60
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-09-20 08:14:46 +0530

    Update Easy_to_pronoun.java

--- Easy_to_pronoun.java
+++ Easy_to_pronoun.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Easy_to_pronoun {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         while (t-- > 0) {
