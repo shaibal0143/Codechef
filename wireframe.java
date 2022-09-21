dd014827e1552d3e9954bd9e2914181453f7fb3e
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-09-21 08:16:43 +0530

    Add files via upload

---
+++ wireframe.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class wireframe {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int x = scan.nextInt();
+            int c = 2 * (n + m) * x;
+            System.out.println(c);
+        }
+        scan.close();
+    }
+
+}
