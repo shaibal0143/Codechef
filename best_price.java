8fa3864a745dff192e1c63524fa235eed6b2a020
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-11-08 22:02:29 +0530

    Update best_price.java

--- best_price.java
+++ best_price.java
@@ -6,7 +6,7 @@ public class best_price {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         while (t-- > 0) {
-            int x = scan.nextInt();
+            int x = scan.nextInt(); 
             int y = scan.nextInt();
             int z = scan.nextInt();
             int a = scan.nextInt();
