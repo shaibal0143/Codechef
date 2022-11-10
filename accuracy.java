fe033a1f4ccdb9065b39c999bf5b465d1f47ad7c
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-11-10 11:16:27 +0530

    Update accuracy.java

--- accuracy.java
+++ accuracy.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class accuracy {
      public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
         while (t-- > 0) {
             int x = scan.nextInt();
