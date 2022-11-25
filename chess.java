cdd218b2a4250545f086270d3168d497d8c06ba8
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-11-25 10:14:18 +0530

    Update chess.java

--- chess.java
+++ chess.java
@@ -6,7 +6,7 @@ public class chess  {
         int t = scan.nextInt();
         for (int i = 1; i <= t; i++) {
             int n = scan.nextInt();
-            int min = n * 60;
+            int min = n * 60; 
             int times = min / 20;
             System.out.println(times);
         }
