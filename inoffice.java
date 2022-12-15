e3f09a6974f357feb4308e3738d63f4ab3677f70
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-15 19:31:53 +0530

    Update inoffice.java

--- inoffice.java
+++ inoffice.java
@@ -6,7 +6,7 @@ public class inoffice {
         int t = scan.nextInt();
         while (t-- > 0) {
             int x = scan.nextInt();
-            int y = scan.nextInt();
+            int y = scan.nextInt(); 
             System.out.println(4 * x + y);
         }
         scan.close();
