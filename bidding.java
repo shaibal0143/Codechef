f56138145152009bfdc3bd6de0da865de3ae75ec
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-18 11:12:49 +0530

    Update bidding.java

--- bidding.java
+++ bidding.java
@@ -6,7 +6,7 @@ public class bidding {
         // your code goes here
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < a; i++) { 
             int A = sc.nextInt(); 
             int B = sc.nextInt();
             int C = sc.nextInt();
