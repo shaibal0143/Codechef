286bc98e5b57bbc0ba434455b34a2d58de6044bd
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-19 20:31:29 +0530

    Update StolenDoll.java

--- StolenDoll.java
+++ StolenDoll.java
@@ -4,7 +4,7 @@ public class StolenDoll {
     public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        while (t-- > 0) { 
+        while (t-- > 0) {  
             int n = scan.nextInt(); 
             int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
