b97f510d2a814ec266aef1b4715ebc0531097839
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-31 08:30:32 +0530

    Update coins_needed.java

--- coins_needed.java
+++ coins_needed.java
@@ -4,7 +4,7 @@ public class coins_needed {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        for (int i = 1; i <= t; i++) {
+        for (int i = 1; i <= t; i++) { 
             int n = scan.nextInt();
             int c = n % 10;
             System.out.println(c);
