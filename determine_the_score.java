08ed39d23eb3f5d744973831079a810153150577
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2023-01-10 07:08:38 +0530

    Update determine_the_score.java

--- determine_the_score.java
+++ determine_the_score.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class determine_the_score {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
         for (int i = 1; i <= t; i++) {
             int x = scan.nextInt(); 
