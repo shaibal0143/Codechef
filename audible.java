cd556227143303eb0cf23d3037aa20ae4de626dd
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2023-02-08 08:36:24 +0530

    Add files via upload

---
+++ audible.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class audible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            if (x >= 67 && x <= 45000) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+        sc.close();
+    }
+
+}
