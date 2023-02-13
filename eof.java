b97b721ec95bb2f868df03499a2cd96a367abe59
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-13 08:28:46 +0530

    Add files via upload

---
+++ eof.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class eof {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner scan = new Scanner(System.in);
+
+        int n = 1;
+        while (scan.hasNext()) {
+            System.out.println(n++ + " " + scan.nextLine());
+        }
+        scan.close();
+    }
+}
