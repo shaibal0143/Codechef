02089608a7e5279671dccf652293c770ecc9fd99
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-15 20:03:56 +0530

    Add files via upload

--- wireframe.java
+++ wireframe.java
@@ -1,17 +1,20 @@
+
 import java.util.Scanner;
 
-public class wireframe {
-    public static void main(String[] args) { 
-        Scanner scan = new Scanner(System.in); 
-        int t = scan.nextInt();
-        while (t-- > 0) {
-            int n = scan.nextInt();
-            int m = scan.nextInt();
-            int x = scan.nextInt(); 
-            int c = 2 * (n + m) * x;
-            System.out.println(c);
-        }
-        scan.close(); 
-    }
+class wireframe {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
+        int t = read.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = read.nextInt();
+            int m = read.nextInt();
+            int x = read.nextInt();
+            // Update the code below this line to solve the problem
+            int sum = 2 * n + 2 * m;
+            System.out.println(sum * x);
+
+        }
+        read.close();
+    }
 }
