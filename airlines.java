4689bf01fc62a806a1a170eedcd9a1cff08f5cad
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-16 08:39:47 +0530

    Add files via upload

---
+++ airlines.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class airlines {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int z = scan.nextInt();
+            int sum = 10 * x;
+            if (sum >= y) {
+                System.out.println(y * z);
+            } else {
+                System.out.println(sum * z);
+            }
+
+        }
+
+    }
+
+}
