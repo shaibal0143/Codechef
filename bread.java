dfa9f0cc4b89800b9b8469e3a99ef1aa6b098cda
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-20 20:14:23 +0530

    Add files via upload

---
+++ bread.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class bread {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int k = scan.nextInt();
+            int s = m * k;
+            if (s >= n) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+}
