b3ad32995f608565a3c60e4ce1adb82c7de99e85
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-22 07:22:16 +0530

    Update construct_n.java

--- construct_n.java
+++ construct_n.java
@@ -5,7 +5,7 @@ public class construct_n e {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int a = scan.nextInt();
-        int b = scan.nextInt();
+        int b = scan.nextInt(); 
         int y = n - a; 
         System.out.println(y);
         System.out.println(y - b);
