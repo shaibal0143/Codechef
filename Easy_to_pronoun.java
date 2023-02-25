4b73627e06feee2376c499be388c70975bf16431
skp <104377565+shaibal0143@users.noreply.github.com>
2023-02-25 20:03:47 +0530

    Update Easy_to_pronoun.java

--- Easy_to_pronoun.java
+++ Easy_to_pronoun.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
         while (t-- > 0) {
             int count = 0;
             int n = scan.nextInt(); 
-            String s = scan.next();
+            String s = scan.next(); 
             for (int i = 0; i < n; i++) { 
                 char ch = s.charAt(i);
                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
