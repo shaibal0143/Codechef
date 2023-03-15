3e04d3280ded3568a7d11f8222a203d38a5b5978
skp <104377565+shaibal0143@users.noreply.github.com>
2023-03-15 07:42:11 +0530

    Add files via upload

---
+++ flip_cards.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class flip_cards {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int x = scan.nextInt();
+            if (x == 0) {
+                System.out.println(0);
+            } else if (x == n) {
+                System.out.println(0);
+            } else if (x < n / 2) {
+                System.out.println(x);
+            } else if (x > n / 2) {
+                System.out.println(n - x);
+            }
+        }
+    }
+}
