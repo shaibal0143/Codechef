1001ade2f14b5343816a7a18f66fd6f17f0c0847
skp <104377565+shaibal0143@users.noreply.github.com>
2023-03-25 09:05:24 +0530

    Update biryani_classes.java

--- biryani_classes.java
+++ biryani_classes.java
@@ -4,7 +4,7 @@ public class biryani_classes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        for (int i = 1; i <= t; i++) {
+        for (int i = 1; i <= t; i++) { 
             int x = scan.nextInt();
             int y = scan.nextInt();
             int s = x * y;
