92883f10a71658c8054a32bcebb65cc2f7046908
skp <104377565+shaibal0143@users.noreply.github.com>
2023-03-31 15:31:16 +0530

    Add files via upload

---
+++ cadies.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class cadies {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = a / b;
+            if ((a % b == 0) && (c % 2 == 0)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+
+    }
+
+}
