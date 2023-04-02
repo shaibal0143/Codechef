b2fc291a8a35674fcc00c66260f693ca880a885d
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-02 08:54:31 +0530

    Update cadies.java

--- cadies.java
+++ cadies.java
@@ -4,7 +4,7 @@ public class cadies {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        while (t-- > 0) {
+        while (t-- > 0) { 
             int a = scan.nextInt();
             int b = scan.nextInt();
             int c = a / b;
