80a5501d06b4be4734dc2f259fdd3357ebb1b338
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-06 09:02:12 +0530

    Add files via upload

---
+++ expence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class expence {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int x = scan.nextInt();
+            int a = 2;
+            double tot;
+            for (int i = 1; i <= n; i++) {
+                // if(i==1){
+                double y = (Math.pow(a, x) - Math.pow(a, x) / 2);
+                tot = y;
+            }
+            System.out.println(tot);
+
+        }
+    }
+}
