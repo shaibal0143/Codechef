6eab816481a9c87065a315852e7be701b80002ab
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-06 09:02:25 +0530

    Update marks.java

--- marks.java
+++ marks.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class marks {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
         while (t-- > 0) {
             int k = scan.nextInt();
