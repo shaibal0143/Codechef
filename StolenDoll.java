1282205bef77c70f6464644db6ece347f3b0bcab
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-12 07:42:48 +0530

    Update StolenDoll.java

--- StolenDoll.java
+++ StolenDoll.java
@@ -6,7 +6,7 @@ public class StolenDoll {
         int t = scan.nextInt();
         while (t-- > 0) {  
             int n = scan.nextInt(); 
-            int arr[] = new int[n]; 
+            int arr[] = new int[n];  
             for (int i = 0; i < n; i++) {
                 arr[i] = scan.nextInt(); 
             }       
