9b729b39de556540741a6c769e9b120c6820e90c
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-20 23:25:48 +0530

    Add files via upload

--- candies.java
+++ candies.java
@@ -1,18 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class candies {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        for (int i = 1; i <= t; i++) {
+        while (t-- > 0) {
             int n = scan.nextInt();
-            int x = scan.nextInt();
-            int diff = n - x;
-            int can = diff / 4;
-            System.out.println(can);
+            int a[] = new int[n * 2];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = scan.nextInt();
+
+            }
+            int count = 0;
+            int count2 = 0;
+            for (int j = 0; j < a.length; j++) {
+
+                count = 0;
+                for (int k = 0; k < a.length; k++) {
+                    if (a[j] == a[k]) {
+
+                        count++;
+                    }
+
+                }
+
+                if (count > 2) {
+                    System.out.println("no");
+                    break;
+                } else {
+                    count2++;
+
+                }
+
+                if (count2 == a.length) {
+                    System.out.println("yes");
+                }
+                scan.close();
+            }
 
         }
-
     }
- 
+
 }
