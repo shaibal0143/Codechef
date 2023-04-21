f657a5bb458c7710b87f8d1d7a543a5493eb776b
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-21 11:04:17 +0530

    Update audible.java

--- audible.java
+++ audible.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class audible {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         int t = sc.nextInt();
         for (int i = 0; i < t; i++) { 
             int x = sc.nextInt();
