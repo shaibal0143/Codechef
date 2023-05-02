b19e81dad7b0cb83bf0d6cd438a534fd8f6c2d5c
skp <104377565+shaibal0143@users.noreply.github.com>
2023-05-02 21:35:09 +0530

    Update add_2_no.java

--- add_2_no.java
+++ add_2_no.java
@@ -7,7 +7,7 @@ public class add_2_no {
         int t = scan.nextInt();
         while (t-- > 0) {
             int a = scan.nextInt(); 
-            int b = scan.nextInt();
+            int b = scan.nextInt(); 
             System.out.println(a + b);
         }
    
