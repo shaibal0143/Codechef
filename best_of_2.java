5012121853db13d6e7a4e4f08f3d4274c17929ea
skp <104377565+shaibal0143@users.noreply.github.com>
2023-05-05 09:07:00 +0530

    Update best_of_2.java

--- best_of_2.java
+++ best_of_2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class best_of_2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
         while (t-- > 0) {
             int a = scan.nextInt(); 
