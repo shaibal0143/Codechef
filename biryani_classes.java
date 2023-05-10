980623b668fc5da181d57ef6cf057027e4711945
skp <104377565+shaibal0143@users.noreply.github.com>
2023-05-10 22:13:44 +0530

    Update and rename biryani_classes.java to biryani_classes.java

--- biryani_classes.java
+++ biryani_classes.java
@@ -5,7 +5,7 @@ public class biryani_classes {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         for (int i = 1; i <= t; i++) { 
-            int x = scan.nextInt();
+            int x = scan.nextInt(); 
             int y = scan.nextInt();
             int s = x * y; 
  
