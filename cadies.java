355c6e3e17cef822ee7738b3641a99fd4b8f97ad
skp <104377565+shaibal0143@users.noreply.github.com>
2023-05-30 10:23:26 +0530

    Update cadies.java

--- cadies.java
+++ cadies.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class cadies {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         int t = scan.nextInt();
         while (t-- > 0) { 
             int a = scan.nextInt(); 
